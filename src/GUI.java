23ff30b13700b4dc3986c6bddd5aa41d50a8fcca
Alexis Zakrzewski <alexisr.zakrzewski@gmail.com>
2023-03-03 14:38:22 -0800

    Added try catch

--- src/GUI.java
+++ src/GUI.java
@@ -72,6 +72,11 @@ public class GUI extends JFrame {
             CellToken cellToken = new CellToken();
             cellToken.setRow(row);
             cellToken.setColumn(col);
+            try {
+                mySheet.changeCellFormulaAndRecalculate(cellToken, formula);
+            } catch (ArrayIndexOutOfBoundsException a) {
+                JOptionPane.showMessageDialog(myTable, "Please select a cell.");
+            }
             mySheet.changeCellFormulaAndRecalculate(cellToken, formula);
             updateAllCells();
             //myTable.setValueAt(formula, row, col);
